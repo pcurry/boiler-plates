291ed6cdd606e9e9b468e2b23badd3650cb53b9d
Paul Curry <pcurry@gmail.com>
2017-11-16 13:07:51 -0500

    Boilerplate for a terraform module vars.tf
    I'm back to doing terraform, and modules are really useful.

---
+++ terraform/module/vars.tf
@@ -0,0 +1,13 @@
+# Required variables first
+
+variable "<first_required>" {
+  description = "<first_required_desc>"
+}
+
+
+# Optional variables last
+
+variable "<first_optional>" {
+  description = "<first_optional_desc>"
+  default = "<first_optional_default"
+}
