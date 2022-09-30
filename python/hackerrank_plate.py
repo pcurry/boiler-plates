5444b750bcdefc7ada053545ba890e5f335f676b
Paul Curry <pcurry@gmail.com>
2022-09-29 21:33:34 -0400

    Hackerrank stopped providing the input parser so have a boiler-plate ready to make them as needed.

---
+++ python/hackerrank_plate.py
@@ -0,0 +1,11 @@
+
+def main():
+    ## "Do what you need to do here"
+    total_inputs = int(input().strip())
+
+    # The per-challenge structure of the arguments will go here
+
+    
+
+if __name__ == '__main__':
+    main()
